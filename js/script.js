1d7dee095056d1a116cd2f9ee0e11fb883626cae
Sharif101 <sharif009009@gmail.com>
2022-08-23 13:48:45 +0600

    player 5

--- js/script.js
+++ js/script.js
@@ -3,11 +3,27 @@
 //     console.log(text);
 // })
 
+
+
 let players = document.getElementsByClassName('btn-player');
+
 
 for (const player of players) {
     player.addEventListener('click', function(event){
         let text = event.target.parentNode.children[0].innerText;
-        console.log(text);
+        let listAll = document.querySelectorAll('#list-body li');
+
+        if(listAll.length <5 ){
+            let listBody = document.getElementById('list-body');
+            player.setAttribute('disabled', 'true');
+
+         let li = document.createElement('li');
+            li.innerText = text;
+            listBody.appendChild(li);
+        }
+        else{
+            alert("error");
+        }
     })
 }
+
